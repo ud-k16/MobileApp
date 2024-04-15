6389a0e96682acf8654afb87eac349745c954fe5
uma <forjust224@gmail.com>
2024-04-15 14:22:48 +0530

    sample data added

--- src/store/sampleData.js
+++ src/store/sampleData.js
@@ -1,15 +1,37 @@
 export const SampleList = {
   emeraldData: [
     {
-      name: '',
+      name: 'Colombian Emerald',
+      placeOfOrigin: 'Colombian',
+      imageUrl: '',
+      description:
+        'These emeralds emit a warm hue, and are of the highest quality in terms of color, size, brilliance and pureness. Green is the primary color of the Columbian emerald, though some variations give the color unique shades of green. They are among the most sought-after emeralds despite having only a small change in color. They range from an intense bright green to a bluish-green color.',
+    },
+    {
+      name: 'Brazilian Emerald',
+      placeOfOrigin: 'Brazilian',
+      imageUrl: '',
+      description:
+        'The first emerald was found in Brazil, in the year 1920. The quality of the emeralds mined in Brazilian mines at the time was not thought to be on par with the best stones on the market. By the end of the 1980s, new mines had been found and are now reputed to produce much better metals than previously available ones.',
+    },
+    {
+      name: 'Zambian Emerald',
+      placeOfOrigin: 'Zambian',
+      imageUrl: '',
+      description:
+        'This African nation is the second-largest producer of this gemstone in the world due to its extensive beryl deposits. It was only recently discovered by humans, around 1950. These Zambian emeralds are a deeper shade of green than the Columbian emerald, which is prized for its leafy green hue. These emeralds have less porosity, are smaller, and less brittle. They have a brilliant luster and appear clearer.Additionally, purchasing stones from Zambia is very affordable. Gems are valued according to where they were mined. Therefore, these Emeralds are significantly more affordable for all classes of buyers and are priced less than their Columbian counterparts on this count.',
+    },
+    {
+      name: 'Synthetic Emerald',
       placeOfOrigin: '',
       imageUrl: '',
-      description: '',
+      description:
+        'In the early 1930s, creating artificial emeralds engaged dissolving gym nutrition in a liquid flux and letting it crystallize when only a small piece of a natural emerald of beryl was present. The emeralds made at that time were of low grade and size, but in the 1960s, a novel technique for making imitation emeralds was developed.Using extremely high temperatures and pressures, crystal nutrients are dissolved in acid with water and certain other chemicals, after which it is allowed to help crystallize inside a cooling chamber. As a result, emeralds made of synthetic materials are much less expensive and resemble real ones in appearance.',
     },
   ],
   emeraldStore: [
     {
-      name: 'uma',
+      name: 'jewel pte ltd.',
       place: 'Cambodia',
       imageUrl: '',
       description:
@@ -17,12 +39,12 @@ export const SampleList = {
       websiteLink: 'https://google.com',
     },
     {
-      name: '',
-      place: '',
+      name: 'EJINDIA pvt ltd',
+      place: 'India',
       imageUrl: '',
       description:
         'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet.."',
-      websiteLink: 'https://google.com',
+      websiteLink: 'https://www.ejindia.com/dealers/silver-dealers',
     },
   ],
 };
